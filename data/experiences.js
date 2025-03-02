d5b9f29f7f3159f9207a1185519d316fccae476f
Béatrice COUDERC <beatrice_couderc@orange.fr>
2025-03-02 18:48:04 +0100

    Ajout des logos réseaux sociaux + chevrons de navigation Création du fichier JSON experiences

---
+++ data/experiences.js
@@ -0,0 +1,58 @@
+[
+    {
+        "id": "1",
+        "date": "2024 à aujourd'hui",
+        "poste": "Technicienne de proximité",
+        "missions": [
+            "Gestion et maintenance du parc informatique",
+            "Support aux utilisateurs et résolution des incidents",
+            "Masterisation et configuration de postes de travail",
+            "Suivi des interventions et documentation",
+            "Gestion du déménagement du site",
+            "Organisation et mise en oeuvre du renouvellement du parc informatique en totale autonomie (70 machines)"
+        ],
+        "titreComp": "Compétences mise en oeuvre",
+        "competences": [
+            "Diagnostic et résolution de problèmes informatiques",
+            "Gestion des tickets d’incidents",
+            "Installation et configuration de systèmes d’exploitation",
+            "Relation client et communication",
+            "Autonomie et sens de l’organisation"
+        ]
+    },
+    {
+        "id": "2",
+        "date": "Février 2023 - Avril 2023",
+        "poste": "Fondation Bon Sauveur, Bégard - Technicienne Système et Réseau",
+        "missions": [
+            "Support Helpdesk et gestion des tickets (GLPI)",
+            "Mise à jour et renouvellement du parc informatique",
+            "Déploiement et configuration des postes de travail"
+        ],
+        "titreComp": "Compétences mise en oeuvre",
+        "competences": [
+            "Utilisation d’un outil ITSM (GLPI)",
+            "Déploiement de postes via image système",
+            "Gestion et mise à jour d’un parc informatique",
+            "Support utilisateur"
+        ]
+    },
+    {
+        "id": "3",
+        "date": "Septembre 2022 - Novembre 2022",
+        "poste": "Cerfrance Côte-d'Armor, Plérin - Technicienne Hotline",
+        "missions": [
+            "Installation et maintenance de l’application ISACOMPTA",
+            "Support aux clients et employés sur le matériel et les logiciels",
+            "Dépannage à distance et gestion des demandes d’impression"
+        ],
+        "titreComp": "Compétences mise en oeuvre",
+        "competences": [
+            "Assistance technique téléphonique et à distance",
+            "Utilisation d’un outil ITSM (GLPI)",
+            "Installation et configuration de logiciels métiers",
+            "Dépannage matériel et logiciel",
+            "Relation client et pédagogie"
+        ]
+    }
+]
